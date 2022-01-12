caadedfb01fe952f71c1ff485fe06a90f2fbba1f
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-11 17:18:54 -0700

    displayworkshops

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://xaqoxrtpghvasocestqf.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTgzNDAxMCwiZXhwIjoxOTU3NDEwMDEwfQ.Hm5b4mWb79EzdIiBol6bx_RrEwHStVieT1oxQED6ZtM';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -16,7 +16,7 @@ export async function checkAuth() {
 
 export async function redirectIfLoggedIn() {
     if (await getUser()) {
-        location.replace('./other-page');
+        location.replace('./workshops');
     }
 }
 
