3813a552cb504017574e34d1d057a3b4b4232d0d
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-12 15:08:37 -0700

    form

--- create/create.js
+++ create/create.js
@@ -7,3 +7,17 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
+form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+    const name = data.get('name');
+    const contact = data.get('contact');
+    const workshop_id = data.get('workshop_id');
+
+    await createParticipant({
+        name: name,
+        contact: contact,
+        workshop_id: workshop_id
+    });
+});
