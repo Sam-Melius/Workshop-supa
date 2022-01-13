a7cd983be1fee4c66b8143b81ae3f92d67aeeb86
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-12 17:07:06 -0700

    test

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,23 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderParticipant } from '../render-utils.js';
 
 const test = QUnit.test;
+
+const participant = [
+    { name: 'sam', contact: 'dude@dude.com' }
+];
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = participant;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderParticipant(participant);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(actual, expected);
 });
